91c270b6e5e701262e6f0f13d2b0b5c148b2080a
Denis Rafi <62852838+DenisError404@users.noreply.github.com>
2020-04-14 17:32:43 +0100

    Add files via upload

---
+++ Print Matrix.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrintMatrix
+{
+    class PrintMatrix
+    {
+        static void Main()
+        {
+            int N;
+            Console.WriteLine("By Denis Rafi");
+            Console.WriteLine("-----------------");
+
+            Console.Write("Enter an integer number < 25:");
+            bool isnInt = int.TryParse(Console.ReadLine(), out N);
+            if (isnInt & N < 25 & N > 0)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 1; j < N + 1; j++)
+                    {
+                        if (j + i > 9)
+                        {
+                            Console.Write("{0} ", j + i);
+                        }
+                        else
+                        {
+                            Console.Write("{0}  ", j + i);
+                        }
+                    }
+                    Console.WriteLine();
+
+
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Wrong entry!");
+
+            }
+        }
+    }
+}
